035a25d8b025b4b5062b35c587272eeb7ae42bc6
bartosjiri <me@bartosjiri.com>
2026-07-18 10:19:18 +0200

    Update robots.txt file source

---
+++ src/routes/robots.txt/+server.ts
@@ -0,0 +1,13 @@
+import { META_DOMAIN } from '@/util/meta';
+
+export const prerender = true;
+
+export const GET = async () => {
+	const body = ['User-agent: *', 'Disallow:', '', `LLMs: ${META_DOMAIN}/llms.txt`].join('\n');
+
+	return new Response(body, {
+		headers: {
+			'Content-Type': 'text/plain; charset=utf-8'
+		}
+	});
+};
