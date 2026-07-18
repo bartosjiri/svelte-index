d745f06483a1db37d3c626f005aab495a54dd267
bartosjiri <me@bartosjiri.com>
2026-07-18 10:25:23 +0200

    Add sitemap

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -3,7 +3,13 @@ import { META_DOMAIN } from '@/util/meta';
 export const prerender = true;
 
 export const GET = async () => {
-	const body = ['User-agent: *', 'Disallow:', '', `LLMs: ${META_DOMAIN}/llms.txt`].join('\n');
+	const body = [
+		'User-agent: *',
+		'Disallow:',
+		'',
+		`Sitemap: ${META_DOMAIN}/sitemap.xml`,
+		`LLMs: ${META_DOMAIN}/llms.txt`
+	].join('\n');
 
 	return new Response(body, {
 		headers: {
