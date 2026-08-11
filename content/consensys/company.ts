f97623fa76aea027ca4693d805e45b7effcbd17d
bartosjiri <me@bartosjiri.com>
2026-08-11 14:47:41 +0200

    Add Consensys

---
+++ content/consensys/company.ts
@@ -0,0 +1,7 @@
+import type { CompanyConfigT } from '../../src/modules/companies';
+
+export const COMPANY: CompanyConfigT = {
+	name: 'Consensys',
+	description: 'Blockchain software company',
+	websiteUrl: 'https://consensys.io'
+};
