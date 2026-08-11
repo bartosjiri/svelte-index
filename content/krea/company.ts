0f80c4d1b2262453b963c697033d116d09ac013e
bartosjiri <me@bartosjiri.com>
2026-08-11 14:47:51 +0200

    Add Krea

---
+++ content/krea/company.ts
@@ -0,0 +1,7 @@
+import type { CompanyConfigT } from '../../src/modules/companies';
+
+export const COMPANY: CompanyConfigT = {
+	name: 'Krea',
+	description: 'AI creative tools',
+	websiteUrl: 'https://krea.ai'
+};
