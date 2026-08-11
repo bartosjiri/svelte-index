7dd86f9e74f78cce6d642441df87544c82a19c12
bartosjiri <me@bartosjiri.com>
2026-08-11 14:48:11 +0200

    Add Yahoo

---
+++ content/yahoo/company.ts
@@ -0,0 +1,7 @@
+import type { CompanyConfigT } from '../../src/modules/companies';
+
+export const COMPANY: CompanyConfigT = {
+	name: 'Yahoo',
+	description: 'News and information portal',
+	websiteUrl: 'https://yahoo.com'
+};
