23c7da7ffdb6084f9338112db953533309ffa92e
bartosjiri <me@bartosjiri.com>
2026-08-11 14:48:01 +0200

    Add Logitech

---
+++ content/logitech/company.ts
@@ -0,0 +1,7 @@
+import type { CompanyConfigT } from '../../src/modules/companies';
+
+export const COMPANY: CompanyConfigT = {
+	name: 'Logitech',
+	description: 'Computer peripherals manufacturer',
+	websiteUrl: 'https://logitech.com'
+};
